4336a9392c138f5da0af4b290e72f6934fb6e74f
leonardo silva <leonardohsilva.dev@gmail.com>
2020-03-04 00:09:11 -0300

    Criação tipo Cliente, conexão bd, mutation adicionarCliente e query para buscar clientes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { GraphQLServer } = require('graphql-yoga')
 const conexao = require('./infraestrutura/conexao')
 const Tabelas = require('./infraestrutura/database/tabelas')
+const operations = require('./infraestrutura/operations')
 
 conexao.connect(erro => {
   if (erro) {
@@ -12,9 +13,15 @@ conexao.connect(erro => {
   Tabelas.init(conexao)
 })
 
+const Clientes = new operations('cliente')
+
 const resolvers = {
   Query: {
-    status: () => "Servidor rodando!"
+    status: () => "Servidor rodando!",
+    clientes: () => Clientes.lista()
+  },
+  Mutation: {
+    adicionarCliente: (root, params) => Clientes.adiciona(params)
   }
 }
 
